d479b25173a09aa6dca4e51a2c80f6ae4a020c42
Oprea Lucian <47993986+Lucian-IoanOprea@users.noreply.github.com>
2020-07-24 21:32:43 +0300

    Add files via upload

---
+++ src/components/ToDOS.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ToDoItem from './ToDoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+
+  render() {
+  //  console.log(this.props.todos);
+    return this.props.todos.map((todo) =>(
+        <ToDoItem key = {todo.id} todo={todo}  markComplete ={this.props.markComplete} />
+    ));
+  }
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos:PropTypes.array.isRequired
+}
+
+export default Todos;
